7e2cdd720b19da39c4719f33da680df8b7e63e51
Bruno Zocatelli <90390478+BrunoZocatelli123@users.noreply.github.com>
2023-07-05 16:18:56 -0300

    teste.py

---
+++ teste.py
@@ -0,0 +1,27 @@
+import streamlit as st 
+import matplotlib.pyplot as plt
+import pandas as pd
+import plotly.express as px
+
+def page1():
+    st.title('Apresentação pessoal')
+    st.write('Conteúdo da página 1')
+
+def page2():
+    st.title('Conteudos Sobre a Cyrela')
+    st.write('Conteúdo da página 2')
+
+def page3():
+    st.title('Apresentação do Case')
+    st.write('Conteúdo da página 3')
+
+# Criando o seletor de abas
+page = st.sidebar.selectbox('Selecione a página', ('Apresentação pessoal', 'Sobre a Cyrela', 'Apresentação do Case'))
+
+# Renderizando a página selecionada
+if page == 'Apresentação pessoal':
+    page1()
+elif page == 'Sobre a Cyrela':
+    page2()
+elif page == 'Apresentação do Case':
+    page3()
